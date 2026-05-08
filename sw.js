68817722dc96e3b8acee933daf346d40bcd0f649
chacharoc14 <chacharoc14@gmail.com>
2026-05-09 00:48:48 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,70 @@
-const CACHE = 'badgeuse-v1';
+const CACHE = 'badgeuse-v2';
+
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
+  './manifest.json'
 ];
 
+// INSTALL
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+  );
+
   self.skipWaiting();
 });
 
+// ACTIVATE
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+
+  e.waitUntil(
+
+    caches.keys().then(keys =>
+
+      Promise.all(
+
+        keys
+          .filter(k => k !== CACHE)
+          .map(k => caches.delete(k))
+
+      )
+    )
+  );
+
   self.clients.claim();
 });
 
+// FETCH
 self.addEventListener('fetch', e => {
+
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+
+    caches.match(e.request)
+
+      .then(cached => {
+
+        // retourne le cache si dispo
+        if(cached) return cached;
+
+        // sinon fetch réseau
+        return fetch(e.request)
+          .then(res => {
+
+            // copie réponse
+            const copy = res.clone();
+
+            // sauvegarde cache
+            caches.open(CACHE)
+              .then(cache => cache.put(e.request, copy));
+
+            return res;
+          })
+
+          // fallback offline
+          .catch(() => caches.match('./index.html'));
+      })
   );
 });
